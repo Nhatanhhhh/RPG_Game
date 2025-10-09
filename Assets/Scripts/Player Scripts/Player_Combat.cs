e8782607cbe885bdc91e0ffe3ea4aeff73710ad1
Nhat_Anh <nguyennhatanhnnacm@gmail.com>
2025-10-10 01:19:32 +0700

    Update new enemy and UI setting off player

---
+++ Assets/Scripts/Player Scripts/Player_Combat.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class Player_Combat : MonoBehaviour
+{
+    public Transform attackPoint;
+    public LayerMask enemyLayer;
+    public StatsUI statsUI;
+
+    public Animator anim;
+    public float cooldown;
+    private float timer;
+
+    private void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    public void Attack()
+    {
+        if (timer <= 0)
+        {
+            anim.SetBool("IsAttacking", true);
+
+            timer = cooldown;
+        }
+    }
+
+    public void DealDamage()
+    {
+        StatsManager.Instance.damage += 1;
+        statsUI.UpdateDamage();
+
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, StatsManager.Instance.weaponRange, enemyLayer);
+
+        foreach (Collider2D enemy in enemies)
+        {
+            enemy.GetComponent<Enemy_Health>().ChangeHealth(-StatsManager.Instance.damage);
+            enemy.GetComponent<Enemy_Knockback>().Knockback(transform, StatsManager.Instance.knockbackForce, StatsManager.Instance.knockbackTime, StatsManager.Instance.stunTime);
+        }
+
+    }
+
+
+    public void FinishAttacking()
+    {
+        anim.SetBool("IsAttacking", false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, StatsManager.Instance.weaponRange);
+    }
+}
